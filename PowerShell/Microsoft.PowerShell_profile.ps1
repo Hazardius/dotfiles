45b83167634a49fbeb649de8444c01ea252cf67d
Marcin Mateusz Hanc <marcin.mateusz.hanc@gmail.com>
2021-09-13 13:07:09 +0200

    Update dotfiles.

--- PowerShell/Microsoft.PowerShell_profile.ps1
+++ PowerShell/Microsoft.PowerShell_profile.ps1
@@ -1,3 +1,12 @@
+# Ensure you have dependencies installed:
+# Install-Module posh-git -Scope CurrentUser
+# Install-Module oh-my-posh -Scope CurrentUser -RequiredVersion 2.0.412
+# Install-Module -Name PSReadLine -Scope CurrentUser -Force -SkipPublisherCheck
+
 Import-Module posh-git
 Import-Module oh-my-posh
 Set-Theme Paradox
+
+# To use it for All Users on Current Host:
+# code $PSHOME\Microsoft.PowerShell_profile.ps1
+# And paste this content there.
